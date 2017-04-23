ead1782e2e0f85c8f8bdc9b9812fd81f04701b92
Siru A <s1a@ucsd.edu>
2017-04-23 13:53:55 -0700

    j

--- ajax/found.js
+++ ajax/found.js
@@ -1,1 +1,9 @@
 /* TODO: Implement the Found AJAX Handler */
+module.exports = {
+  post: function (req, res){
+    req.body.lat = parseFloat(req.body.lat);
+    req.body.lng = parseFloat(req.body.lng);/*user will enter a string which we needs to convert to float*/
+    require("./data.json").push(req.body);/*push is a function in array that can add a new data to the end of array*/
+    res.success();
+  }
+}
--- ajax/lost.js
+++ ajax/lost.js
@@ -1,1 +1,11 @@
 /* TODO: Implement the lost AJAX Handler */
+module.exports = {
+  get/*name of the handler*/: function (req, res){
+    try{
+      var data = require("./data.json");
+      res.success(data);
+    } catch (ex){
+      res.error(1, "data not found");/*0 represents the success*/
+    }
+  }
+}
